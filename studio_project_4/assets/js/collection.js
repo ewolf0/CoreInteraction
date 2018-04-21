048de754adeb8d1f899e5465d991e31e62a0a9ed
ellie <wolfe175@newschool.edu>
2018-04-21 16:05:17 -0400

    update

--- studio_project_4/assets/js/collection.js
+++ studio_project_4/assets/js/collection.js
@@ -11,10 +11,20 @@ $.getJSON(url, function(data){
 
 	for (var i = 0; i < data.mycollection.length; i++) {
 
-		$("#info").append("<p>" + data.mycollection[i].imageurl + "</p>")
-		$("#info").append("<div class='aclass'><div>new class</div><img src='" + data.mycollection[i].imageurl + "'></div>")
+		// $("#info").append("<p>" + data.mycollection[i].imageurl + "</p>")
+				//^^ gets the names of each imgurl to show up as <p> tags
+		// $("#info").append("<div class='allitems'><div>new</div><img src='" + data.mycollection[i].imageurl + "'></div>")
 
 		// where is says "body" --> you can attach it to anything like any div to have the info show up in a specific area
+
+		// $("#info").append("<div id='item_" + i + "' class='allitems'><div>new</div><img src='" + data.mycollection[i].imageurl + "'></div>")
+			// this allows me to target one specific item
+	
+
+		$("#info").append("<div id='item_" + i + "' class='allitems'></div>")
+		$("#item_" + i ).append("<p>" + data.mycollection[i].title + "</p>")
+		$("#item_" + i ).append("<div>new</div>")
+		$("#item_" + i ).append("<img src='" + data.mycollection[i].imageurl + "'>")
 
 	}
 })
